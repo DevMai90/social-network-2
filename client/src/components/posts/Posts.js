5d1e60ae57cb06263e9cdbbf928b3bba73d16c62
DevMai90 <dmai90@yahoo.com>
2019-05-09 10:52:47 -0700

    Created Posts component, getPosts action, type, and reducer.

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+import { pathToFileURL } from 'url';
+
+const Posts = ({ post: { posts, loading }, getPosts }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return <div>hi</div>;
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
