d933cd21a3d6392048f18058f6d8d93e9b4841bf
DevMai90 <dmai90@yahoo.com>
2019-05-09 11:20:45 -0700

    Created PostItem component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,13 +3,28 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
-import { pathToFileURL } from 'url';
+import PostItem from './PostItem';
 
 const Posts = ({ post: { posts, loading }, getPosts }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>hi</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      {/* Post Form */}
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
