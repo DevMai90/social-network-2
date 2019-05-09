108709c899e4bee7e87d2cb9f15325077703098c
DevMai90 <dmai90@yahoo.com>
2019-05-08 21:40:48 -0700

    Added ProfileExperience component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 const Profile = ({
   match,
@@ -36,6 +37,18 @@ const Profile = ({
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(exp => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
