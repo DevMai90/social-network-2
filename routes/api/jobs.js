7da59a4bd247cab52faabe92f1f785e133b1d2f2
DevMai90 <dmai90@yahoo.com>
2019-05-31 12:51:11 -0700

    Added field input validation on backend

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 
 // Load Models
@@ -9,29 +10,44 @@ const Job = require('../../models/Job');
 // @route   POST /api/jobs
 // @desc    Create a job post
 // @access  Private
-router.post('/', auth, async (req, res) => {
-  // @todo: Add input validation errors
-  const {
-    seniority,
-    term,
-    position,
-    skills,
-    description,
-    location,
-    salary,
-    benefits,
-    contactName,
-    contactInfo,
-    availability
-  } = req.body;
-
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-
-    const newJob = new Job({
-      user: req.user.id,
-      name: user.name,
-      avatar: user.avatar,
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('seniority', 'Seniority field is required')
+        .not()
+        .isEmpty(),
+      check('term', 'Term field is required')
+        .not()
+        .isEmpty(),
+      check('position', 'Position field is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills field is required')
+        .not()
+        .isEmpty(),
+      check('description', 'Description field is required')
+        .not()
+        .isEmpty(),
+      check('location', 'Location field is required')
+        .not()
+        .isEmpty(),
+      check('contactName', 'Contact name is required')
+        .not()
+        .isEmpty(),
+      check('contactInfo', 'Contact information is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // @todo: Add input validation errors
+    const {
       seniority,
       term,
       position,
@@ -43,16 +59,37 @@ router.post('/', auth, async (req, res) => {
       contactName,
       contactInfo,
       availability
-    });
+    } = req.body;
 
-    const job = await newJob.save();
+    try {
+      const user = await User.findById(req.user.id).select('-password');
 
-    // console.log(user);
-    res.json(job);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send('Server Error');
+      const newJob = new Job({
+        user: req.user.id,
+        name: user.name,
+        avatar: user.avatar,
+        seniority,
+        term,
+        position,
+        skills,
+        description,
+        location,
+        salary,
+        benefits,
+        contactName,
+        contactInfo,
+        availability
+      });
+
+      const job = await newJob.save();
+
+      // console.log(user);
+      res.json(job);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
   }
-});
+);
 
 module.exports = router;
