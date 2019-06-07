50d21fbfd65178fece912652eeb4fd1743213e0e
DevMai90 <dmai90@yahoo.com>
2019-06-06 19:09:44 -0700

    Updated footer links with noopener and noreferrer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,16 +4,36 @@ const Footer = () => {
   return (
     <footer className="footer bg-dark">
       <p className="px text-light">Created by David Mai</p>
-      <a href="http://david-mai.com" className="px text-light">
+      <a
+        href="http://david-mai.com"
+        className="px text-light"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fas fa-globe" />
       </a>
-      <a href="https://github.com/DevMai90" className="px text-light">
+      <a
+        href="https://github.com/DevMai90"
+        className="px text-light"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-github" />
       </a>
-      <a href="https://linkedin.com/in/nldavidmai" className="px text-light">
+      <a
+        href="https://linkedin.com/in/nldavidmai"
+        className="px text-light"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-linkedin" />
       </a>
-      <a href="https://twitter.com/devmai90" className="px text-light">
+      <a
+        href="https://twitter.com/devmai90"
+        className="px text-light"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-twitter" />
       </a>
     </footer>
